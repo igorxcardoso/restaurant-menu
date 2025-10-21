acb1bfe1eaf46ea0dd268e3280884e9362f3178d
Igor Cardoso <iygorsilva@gmail.com>
2025-10-21 15:56:39 -0300

    feat(api): add menu model with associations, validations and migration

--- restaurant-menu-api/db/schema.rb
+++ restaurant-menu-api/db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_21_184928) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_21_185426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.bigint "restaurant_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "menus", "restaurants"
 end
